62fcee84e21efdda1e077c7f0599843bbfaea859
deependran7 <deependran.neupane@gmail.com>
2023-12-24 13:30:33 +0545

    First one

---
+++ Parallel Inverter Virtual Impedance/Inverter_Controller1.m
@@ -0,0 +1,325 @@
+function [sys,x0,str,ts,simStateCompliance] = Inverter_Controller(t,x,u,flag,Lf,Cf)
+%SFUNTMPL General MATLAB S-Function Template
+%   With MATLAB S-functions, you can define you own ordinary differential
+%   equations (ODEs), discrete system equations, and/or just about
+%   any type of algorithm to be used within a Simulink block diagram.
+%
+%   The general form of an MATLAB S-function syntax is:
+%       [SYS,X0,STR,TS,SIMSTATECOMPLIANCE] = SFUNC(T,X,U,FLAG,P1,...,Pn)
+%
+%   What is returned by SFUNC at a given point in time, T, depends on the
+%   value of the FLAG, the current state vector, X, and the current
+%   input vector, U.
+%
+%   FLAG   RESULT             DESCRIPTION
+%   -----  ------             --------------------------------------------
+%   0      [SIZES,X0,STR,TS]  Initialization, return system sizes in SYS,
+%                             initial state in X0, state ordering strings
+%                             in STR, and sample times in TS.
+%   1      DX                 Return continuous state derivatives in SYS.
+%   2      DS                 Update discrete states SYS = X(n+1)
+%   3      Y                  Return outputs in SYS.
+%   4      TNEXT              Return next time hit for variable step sample
+%                             time in SYS.
+%   5                         Reserved for future (root finding).
+%   9      []                 Termination, perform any cleanup SYS=[].
+%
+%
+%   The state vectors, X and X0 consists of continuous states followed
+%   by discrete states.
+%
+%   Optional parameters, P1,...,Pn can be provided to the S-function and
+%   used during any FLAG operation.
+%
+%   When SFUNC is called with FLAG = 0, the following information
+%   should be returned:
+%
+%      SYS(1) = Number of continuous states.
+%      SYS(2) = Number of discrete states.
+%      SYS(3) = Number of outputs.
+%      SYS(4) = Number of inputs.
+%               Any of the first four elements in SYS can be specified
+%               as -1 indicating that they are dynamically sized. The
+%               actual length for all other flags will be equal to the
+%               length of the input, U.
+%      SYS(5) = Reserved for root finding. Must be zero.
+%      SYS(6) = Direct feedthrough flag (1=yes, 0=no). The s-function
+%               has direct feedthrough if U is used during the FLAG=3
+%               call. Setting this to 0 is akin to making a promise that
+%               U will not be used during FLAG=3. If you break the promise
+%               then unpredictable results will occur.
+%      SYS(7) = Number of sample times. This is the number of rows in TS.
+%
+%
+%      X0     = Initial state conditions or [] if no states.
+%
+%      STR    = State ordering strings which is generally specified as [].
+%
+%      TS     = An m-by-2 matrix containing the sample time
+%               (period, offset) information. Where m = number of sample
+%               times. The ordering of the sample times must be:
+%
+%               TS = [0      0,      : Continuous sample time.
+%                     0      1,      : Continuous, but fixed in minor step
+%                                      sample time.
+%                     PERIOD OFFSET, : Discrete sample time where
+%                                      PERIOD > 0 & OFFSET < PERIOD.
+%                     -2     0];     : Variable step discrete sample time
+%                                      where FLAG=4 is used to get time of
+%                                      next hit.
+%
+%               There can be more than one sample time providing
+%               they are ordered such that they are monotonically
+%               increasing. Only the needed sample times should be
+%               specified in TS. When specifying more than one
+%               sample time, you must check for sample hits explicitly by
+%               seeing if
+%                  abs(round((T-OFFSET)/PERIOD) - (T-OFFSET)/PERIOD)
+%               is within a specified tolerance, generally 1e-8. This
+%               tolerance is dependent upon your model's sampling times
+%               and simulation time.
+%
+%               You can also specify that the sample time of the S-function
+%               is inherited from the driving block. For functions which
+%               change during minor steps, this is done by
+%               specifying SYS(7) = 1 and TS = [-1 0]. For functions which
+%               are held during minor steps, this is done by specifying
+%               SYS(7) = 1 and TS = [-1 1].
+%
+%      SIMSTATECOMPLIANCE = Specifices how to handle this block when saving and
+%                           restoring the complete simulation state of the
+%                           model. The allowed values are: 'DefaultSimState',
+%                           'HasNoSimState' or 'DisallowSimState'. If this value
+%                           is not speficified, then the block's compliance with
+%                           simState feature is set to 'UknownSimState'.
+
+
+%   Copyright 1990-2010 The MathWorks, Inc.
+
+%
+% The following outlines the general structure of an S-function.
+%
+
+switch flag,
+
+  %%%%%%%%%%%%%%%%%%
+  % Initialization %
+  %%%%%%%%%%%%%%%%%%
+  case 0,
+    [sys,x0,str,ts,simStateCompliance]=mdlInitializeSizes;
+
+  %%%%%%%%%%%%%%%
+  % Derivatives %
+  %%%%%%%%%%%%%%%
+  case 1,
+    sys=mdlDerivatives(t,x,u,Cf);
+
+  %%%%%%%%%%
+  % Update %
+  %%%%%%%%%%
+  case 2,
+    sys=mdlUpdate(t,x,u);
+
+  %%%%%%%%%%%
+  % Outputs %
+  %%%%%%%%%%%
+  case 3,
+    sys=mdlOutputs(t,x,u,Lf,Cf);
+
+  %%%%%%%%%%%%%%%%%%%%%%%
+  % GetTimeOfNextVarHit %
+  %%%%%%%%%%%%%%%%%%%%%%%
+  case 4,
+    sys=mdlGetTimeOfNextVarHit(t,x,u);
+
+  %%%%%%%%%%%%%
+  % Terminate %
+  %%%%%%%%%%%%%
+  case 9,
+    sys=mdlTerminate(t,x,u);
+
+  %%%%%%%%%%%%%%%%%%%%
+  % Unexpected flags %
+  %%%%%%%%%%%%%%%%%%%%
+  otherwise
+    DAStudio.error('Simulink:blocks:unhandledFlag', num2str(flag));
+
+end
+
+% end sfuntmpl
+
+%
+%=============================================================================
+% mdlInitializeSizes
+% Return the sizes, initial conditions, and sample times for the S-function.
+%=============================================================================
+%
+function [sys,x0,str,ts,simStateCompliance]=mdlInitializeSizes
+
+%
+% call simsizes for a sizes structure, fill it in and convert it to a
+% sizes array.
+%
+% Note that in this example, the values are hard coded.  This is not a
+% recommended practice as the characteristics of the block are typically
+% defined by the S-function parameters.
+%
+sizes = simsizes;
+
+sizes.NumContStates  = 4;
+sizes.NumDiscStates  = 0;
+sizes.NumOutputs     = 3;
+sizes.NumInputs      = 8;
+sizes.DirFeedthrough = 1;
+sizes.NumSampleTimes = 1;   % at least one sample time is needed
+
+sys = simsizes(sizes);
+
+%
+% initialize the initial conditions
+%
+x0  = [0 0 0 0];
+
+%
+% str is always an empty matrix
+%
+str = [];
+
+%
+% initialize the array of sample times
+%
+ts  = [0 0];
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+simStateCompliance = 'UnknownSimState';
+
+% end mdlInitializeSizes
+
+%
+%=============================================================================
+% mdlDerivatives
+% Return the derivatives for the continuous states.
+%=============================================================================
+%
+function sys=mdlDerivatives(t,x,u,Cf)
+    %Cf = 50e-6;
+    Kpv = 0.2858615535240143;
+    Kiv =  594.8498590345745;
+    Lv1=0.3e-3;
+    Rv1=0.01;
+
+  gammad1 = x(1);
+  gammaq1 = x(2);
+  
+  vod1 = u(1);
+  voq1 = u(2);
+  iod1 = u(3);
+  ioq1 = u(4);
+  ild1 = u(5);
+  ilq1 = u(6);
+  w1 = u(7);
+  %Virtual impedance
+  vodref1in = u(8);
+  voqref1in = 0;
+  %calulate vodref1 and voqref1
+  vodref1 = vodref1in - iod1*Rv1 + ioq1*w1*Lv1;
+  voqref1 = voqref1in - ioq1*Rv1 - iod1*w1*Lv1;
+  
+ ifdref1  = Kpv*(vodref1 - vod1) + Kiv*gammad1 + iod1 - w1*Cf*voq1;
+ ifqref1  = Kpv*(voqref1 - voq1) + Kiv*gammaq1 + ioq1 - w1*Cf*vod1; 
+ gammad1_dot = vodref1 - vod1;
+ gammaq1_dot = voqref1 - voq1;
+ zetad1_dot = ifdref1 - ild1;
+ zetaq1_dot = ifqref1 - ilq1;
+    
+sys = [gammad1_dot,gammaq1_dot,zetad1_dot,zetaq1_dot];
+
+% end mdlDerivatives
+
+%
+%=============================================================================
+% mdlUpdate
+% Handle discrete state updates, sample time hits, and major time step
+% requirements.
+%=============================================================================
+%
+function sys=mdlUpdate(t,x,u)
+
+sys = [];
+
+% end mdlUpdate
+
+%
+%=============================================================================
+% mdlOutputs
+% Return the block outputs.
+%=============================================================================
+%
+function sys=mdlOutputs(t,x,u,Lf,Cf)
+  %Lf = 3.5e-3;
+  %Cf = 50e-6;
+  Kpv = 0.2858615535240143;
+  Kiv =  594.8498590345745;
+  Kpc = 54.977871437821385;
+  Kic =  1570.7963267948967;
+  Lv=0.35e-3;
+  Rv=0.05; 
+  
+  gammad1 = x(1);
+  gammaq1 = x(2);
+  zetad1 = x(3);
+  zetaq1 =x(4);
+  
+  vod1 = u(1);
+  voq1 = u(2);
+  iod1 = u(3);
+  ioq1 = u(4);
+  ild1 = u(5);
+  ilq1 = u(6);
+  w1 = u(7);
+  %Virtual impedance
+  vodref1in = u(8);
+  voqref1in = 0;
+  %calulate vodref1 and voqref1
+  vodref1 = vodref1in - iod1*Rv + ioq1*w1*Lv;
+  voqref1 = voqref1in - ioq1*Rv - iod1*w1*Lv;
+  
+  ifdref1  = Kpv*(vodref1 - vod1) + Kiv*gammad1 + iod1 - w1*Cf*voq1;
+  ifqref1  = Kpv*(voqref1 - voq1) + Kiv*gammaq1 + ioq1 - w1*Cf*vod1;   
+  vfdref1 = Kpc*(ifdref1 - ild1) + Kic*zetad1 + vod1 - w1*Lf*ilq1;
+  vfqref1 = Kpc*(ifqref1 - ilq1) + Kic*zetaq1 + voq1 + w1*Lf*ild1;
+sys = [vfdref1 vfqref1 0.0];
+
+% end mdlOutputs
+
+%
+%=============================================================================
+% mdlGetTimeOfNextVarHit
+% Return the time of the next hit for this block.  Note that the result is
+% absolute time.  Note that this function is only used when you specify a
+% variable discrete-time sample time [-2 0] in the sample time array in
+% mdlInitializeSizes.
+%=============================================================================
+%
+function sys=mdlGetTimeOfNextVarHit(t,x,u)
+
+sampleTime = 1;    %  Example, set the next hit to be one second later.
+sys = t + sampleTime;
+
+% end mdlGetTimeOfNextVarHit
+
+%
+%=============================================================================
+% mdlTerminate
+% Perform any end of simulation tasks.
+%=============================================================================
+%
+function sys=mdlTerminate(t,x,u)
+
+sys = [];
+
+% end mdlTerminate
---
+++ Parallel Inverter Without Virtual Impedance/Inverter_model.py
@@ -0,0 +1,196 @@
+import numpy as np
+pi = np.pi
+Vdc = 700;
+Lf, Rf, Cf,Rc1, Lc1,Rc2,Lc2 = 3.5E-3,0.1,50E-6,0.01,0.35E-3,0.05,0.8E-3;
+
+#T_inv = 0.001;
+#K_inv = 0.5
+#rN = 1000;
+def Inverter_Controller(t,x,RL,XL,m1,m2,n1,n2,Kpc,Kic,Kpv,Kiv):
+    vdref = 400*np.sqrt(2/3)
+    vqref = 0
+    wf,w0 = 4*np.pi, 2*np.pi*50
+
+    Pinv1,Qinv1,delta1,ild1,ilq1,vod1,voq1,iod1,ioq1,gammad1,gammaq1,zetad1,zetaq1,Pinv2,Qinv2,delta2,ild2,ilq2,vod2,voq2,iod2,ioq2,gammad2,gammaq2,zetad2,zetaq2 = x
+    #Load equation
+    ioD1, ioQ1 = -np.sin(delta1)*iod1 - np.cos(delta1)*ioq1, np.cos(delta1)*iod1 - np.sin(delta1)*ioq1
+    ioD2, ioQ2 = -np.sin(delta2)*iod2 - np.cos(delta2)*ioq2, np.cos(delta2)*iod2 - np.sin(delta2)*ioq2
+    
+    vbD = (ioD1+ioD2)*RL - (ioQ1 + ioQ2)*XL
+    vbQ = (ioD1 + ioD2)*XL + (ioQ1 + ioQ2)*RL
+    #grid model
+    #vbD = rN*(ioD1+ioD2 - iloadD)
+    #vbQ = rN*(ioQ1+ioQ2 - iloadQ)
+    vbd1,vbq1 = -np.sin(delta1)*vbD + np.cos(delta1)*vbQ, -np.cos(delta1)*vbD - np.sin(delta1)*vbQ
+    vbd2,vbq2 = -np.sin(delta2)*vbD + np.cos(delta2)*vbQ, -np.cos(delta2)*vbD - np.sin(delta2)*vbQ
+    #controller
+    pinv1 = (3/2)*(vod1*iod1 + voq1*ioq1)
+    qinv1 = (3/2)*(voq1*iod1 - vod1*ioq1)
+    Pinv1_dot = - wf*Pinv1 + wf*pinv1 
+    Qinv1_dot = - wf*Qinv1 + wf*qinv1 
+    w1  = w0 - Pinv1 * m1
+    delta1_dot = w1 - w1
+    vodref1 = vdref - Qinv1*n1
+    voqref1 = vqref;
+
+    ifdref1  = Kpv*(vodref1 - vod1) + Kiv*gammad1 + iod1 - w1*Cf*voq1
+    ifqref1  = Kpv*(voqref1 - voq1) + Kiv*gammaq1 + ioq1 - w1*Cf*vod1
+    gammad1_dot = vodref1 - vod1
+    gammaq1_dot = voqref1 - voq1
+    zetad1_dot = ifdref1 - ild1
+    zetaq1_dot = ifqref1 - ilq1
+    vfdref1 = Kpc*(ifdref1 - ild1) + Kic*zetad1 + vod1 - w1*Lf*ilq1
+    vfqref1 = Kpc*(ifqref1 - ilq1) + Kic*zetaq1 + voq1 + w1*Lf*ild1
+    vid1,viq1 = np.array([vfdref1,vfqref1])
+    #vid1_dot = (1/T_inv)*(-vid1 + vfdref1*Vdc*K_inv)
+    #viq1_dot = (1/T_inv)*(-viq1 + vfqref1*Vdc*K_inv)
+    ild1_dot = (-Rf/Lf)*ild1 + w1*ilq1 + (vid1 - vod1)/Lf
+    ilq1_dot = -w1*ild1 + (-Rf/Lf)*ilq1 + (viq1 - voq1)/Lf
+    vod1_dot = w1*voq1 + (ild1 - iod1)/Cf
+    voq1_dot = -w1*vod1 + (ilq1 - ioq1)/Cf
+    iod1_dot = (-Rc1/Lc1)*iod1 + w1*ioq1 + (vod1 - vbd1)/Lc1
+    ioq1_dot = -w1*iod1 + (-Rc1/Lc1)*ioq1 + (voq1 - vbq1)/Lc1
+
+
+    #for inverter 2
+    pinv2 = (3/2)*(vod2*iod2 + voq2*ioq2)
+    qinv2 = (3/2)*(voq2*iod2 - vod2*ioq2)
+    Pinv2_dot = - wf*Pinv2 + wf*pinv2
+    Qinv2_dot = - wf*Qinv2 + wf*qinv2 
+    w2 = w0 - Pinv2 * m2
+    delta2_dot = w2 - w1
+    vodref2 = vdref - Qinv2*n2
+    voqref2 = vqref;
+
+    ifdref2  = Kpv*(vodref2 - vod2) + Kiv*gammad2 + iod2 - w2*Cf*voq2
+    ifqref2  = Kpv*(voqref2 - voq2) + Kiv*gammaq2 + ioq2 - w2*Cf*vod2
+    gammad2_dot = vodref2 - vod2
+    gammaq2_dot = voqref2 - voq2
+    zetad2_dot = ifdref2 - ild2
+    zetaq2_dot = ifqref2 - ilq2
+    vfdref2 = Kpc*(ifdref2 - ild2) + Kic*zetad2 + vod2 - w2*Lf*ilq2
+    vfqref2 = Kpc*(ifqref2 - ilq2) + Kic*zetaq2 + voq2 + w2*Lf*ild2
+    vid2,viq2 = np.array([vfdref2,vfqref2])
+    #vid2_dot = (1/T_inv)*(-vid2 + vfdref2*Vdc*K_inv)
+    #viq2_dot = (1/T_inv)*(-viq2 + vfqref2*Vdc*K_inv)
+    ild2_dot = (-Rf/Lf)*ild2 + w2*ilq2 + (vid2 - vod2)/Lf
+    ilq2_dot = -w2*ild2 + (-Rf/Lf)*ilq2 + (viq2 - voq2)/Lf
+    vod2_dot = w2*voq2 + (ild2 - iod2)/Cf
+    voq2_dot = -w2*vod2 + (ilq2 - ioq2)/Cf
+    iod2_dot = (-Rc2/Lc2)*iod2 + w2*ioq2 + (vod2 - vbd2)/Lc2
+    ioq2_dot = -w2*iod2 + (-Rc2/Lc2)*ioq2 + (voq2 - vbq2)/Lc2
+
+    #Load modelling
+    #iloadD_dot = (-w0*RL/XL)*iloadD + w0*iloadQ + (w0/XL)*vbD;
+    #iloadQ_dot = (-w0*RL/XL)*iloadQ - w0*iloadD + (w0/XL)*vbQ;
+
+    return np.array([Pinv1_dot,Qinv1_dot,delta1_dot,ild1_dot,ilq1_dot,vod1_dot,voq1_dot,iod1_dot,ioq1_dot,gammad1_dot,gammaq1_dot,zetad1_dot,zetaq1_dot,Pinv2_dot,Qinv2_dot,delta2_dot,ild2_dot,ilq2_dot,vod2_dot,voq2_dot,iod2_dot,ioq2_dot,gammad2_dot,gammaq2_dot,zetad2_dot,zetaq2_dot])
+
+from sympy import symbols,Matrix,diff, sqrt,print_latex,zeros, sin,cos
+
+# define symbols
+#Inverter Symbols
+vid1,viq1,ild1,ilq1,vod1,voq1,iod1,ioq1 = symbols(['v_id1','v_iq1','i_ld1','i_lq1','v_od1','v_oq1','i_od1','i_oq1'])
+vid2,viq2,ild2,ilq2,vod2,voq2,iod2,ioq2 = symbols(['v_id2','v_iq2','i_ld2','i_lq2','v_od2','v_oq2','i_od2','i_oq2'])
+wf,w0 = symbols(['omega_f','omega_0'])
+vdref,vqref = symbols(['v_d_ref','v_q_ref'])
+m1,m2,n1,n2 = symbols(['m_1','m_2','n_1','n_2'])
+gammad1,gammad2,zetad1,zetad2,gammaq1,gammaq2,zetaq1,zetaq2 = symbols(['gamma_d1','gamma_d2','zeta_d1','zeta_d2','gamma_q1','gamma_q2','zeta_q1','zeta_q2'])
+delta1,delta2,wf,Pinv1,Pinv2,Qinv1,Qinv2 = symbols(['delta1','delta2','w_f','P_inv1','P_inv2','Q_inv1','Q_inv2'])
+
+def get_matrix(der,stat):
+    mat = zeros(len(der),len(stat))
+    for i in range(len(der)):
+        for j in range(len(stat)):
+            mat[i,j] = diff(der[i],stat[j])
+    return mat
+
+from sympy import symbols,Matrix,diff, sqrt,print_latex,zeros, sin,cos
+
+def Inverter_Linear(RL,XL,m1,m2,n1,n2,Kpc,Kic,Kpv,Kiv):
+        #Load equation
+    ioD1, ioQ1 = -sin(delta1)*iod1 - cos(delta1)*ioq1, cos(delta1)*iod1 - sin(delta1)*ioq1
+    ioD2, ioQ2 = -sin(delta2)*iod2 - cos(delta2)*ioq2, cos(delta2)*iod2 - sin(delta2)*ioq2
+    vbD = (ioD1+ioD2)*RL - (ioQ1 + ioQ2)*XL
+    vbQ = (ioD1 + ioD2)*XL + (ioQ1 + ioQ2)*RL
+    vbd1,vbq1 = -sin(delta1)*vbD + cos(delta1)*vbQ, -cos(delta1)*vbD - sin(delta1)*vbQ
+    vbd2,vbq2 = -sin(delta2)*vbD + cos(delta2)*vbQ, -cos(delta2)*vbD - sin(delta2)*vbQ
+
+    #For inverter
+    pinv1 = (3/4)*(vod1*iod1 + voq1*ioq1)
+    qinv1 = (3/4)*(voq1*iod1 - vod1*ioq1)
+    Pinv1_dot = - wf*Pinv1 + wf*pinv1 
+    Qinv1_dot = - wf*Qinv1 + wf*qinv1 
+    w1  = w0 - Pinv1 * m1
+    delta1_dot = w1 - w1
+    vodref1 = vdref - Qinv1*n1
+    voqref1 = vqref;
+
+    ifdref1  = Kpv*(vodref1 - vod1) + Kiv*gammad1 + iod1 - w1*Cf*voq1
+    ifqref1  = Kpv*(voqref1 - voq1) + Kiv*gammaq1 + ioq1 - w1*Cf*vod1
+    gammad1_dot = vodref1 - vod1
+    gammaq1_dot = voqref1 - voq1
+    zetad1_dot = ifdref1 - ild1
+    zetaq1_dot = ifqref1 - ilq1
+    vfdref1 = Kpc*(ifdref1 - ild1) + Kic*zetad1 + vod1 - w1*Lf*ilq1
+    vfqref1 = Kpc*(ifqref1 - ilq1) + Kic*zetaq1 + voq1 + w1*Lf*ild1
+
+    vid1,viq1 = vfdref1,vfqref1
+    #vid1_dot = (1/T_inv)*(-vid1 + vfdref1*Vdc*K_inv)
+    #viq1_dot = (1/T_inv)*(-viq1 + vfqref1*Vdc*K_inv)
+    ild1_dot = (-Rf/Lf)*ild1 + w1*ilq1 + (vid1 - vod1)/Lf
+    ilq1_dot = -w1*ild1 + (-Rf/Lf)*ilq1 + (viq1 - voq1)/Lf
+    vod1_dot = w1*voq1 + (ild1 - iod1)/Cf
+    voq1_dot = -w1*vod1 + (ilq1 - ioq1)/Cf
+    iod1_dot = (-Rc1/Lc1)*iod1 + w1*ioq1 + (vod1 - vbd1)/Lc1
+    ioq1_dot = -w1*iod1 + (-Rc1/Lc1)*ioq1 + (voq1 - vbq1)/Lc1
+
+
+    #for inverter 2
+    pinv2 = (3/4)*(vod2*iod2 + voq2*ioq2)
+    qinv2 = (3/4)*(voq2*iod2 - vod2*ioq2)
+    Pinv2_dot = - wf*Pinv2 + wf*pinv2
+    Qinv2_dot = - wf*Qinv2 + wf*qinv2 
+    w2 = w0 - Pinv2 * m2
+    delta2_dot = w2 - w1
+    vodref2 = vdref - Qinv2*n2
+    voqref2 = vqref;
+
+    ifdref2  = Kpv*(vodref2 - vod2) + Kiv*gammad2 + iod2 - w2*Cf*voq2
+    ifqref2  = Kpv*(voqref2 - voq2) + Kiv*gammaq2 + ioq2 - w2*Cf*vod2
+    gammad2_dot = vodref2 - vod2
+    gammaq2_dot = voqref2 - voq2
+    zetad2_dot = ifdref2 - ild2
+    zetaq2_dot = ifqref2 - ilq2
+    vfdref2 = Kpc*(ifdref2 - ild2) + Kic*zetad2 + vod2 - w2*Lf*ilq2
+    vfqref2 = Kpc*(ifqref2 - ilq2) + Kic*zetaq2 + voq2 + w2*Lf*ild2
+    vid2,viq2 = vfdref2,vfqref2
+    #vid2_dot = (1/T_inv)*(-vid2 + vfdref2*Vdc*K_inv)
+    #viq2_dot = (1/T_inv)*(-viq2 + vfqref2*Vdc*K_inv)
+    ild2_dot = (-Rf/Lf)*ild2 + w2*ilq2 + (vid2 - vod2)/Lf
+    ilq2_dot = -w2*ild2 + (-Rf/Lf)*ilq2 + (viq2 - voq2)/Lf
+    vod2_dot = w2*voq2 + (ild2 - iod2)/Cf
+    voq2_dot = -w2*vod2 + (ilq2 - ioq2)/Cf
+    iod2_dot = (-Rc2/Lc2)*iod2 + w2*ioq2 + (vod2 - vbd2)/Lc2
+    ioq2_dot = -w2*iod2 + (-Rc2/Lc2)*ioq2 + (voq2 - vbq2)/Lc2
+
+    Xinv = Matrix([    Pinv1,     Qinv1,   delta1, ild1,   ilq1,     vod1,    voq1,    iod1,   ioq1,   gammad1,     gammaq1,    zetad1,    zetaq1,    Pinv2,    Qinv2,    delta2,   ild2,     ilq2,    vod2,   voq2,    iod2,    ioq2,    gammad2,     gammaq2,   zetad2,    zetaq2])
+    Xinv_dot = Matrix([Pinv1_dot,Qinv1_dot,delta1_dot,ild1_dot,ilq1_dot,vod1_dot,voq1_dot,iod1_dot,ioq1_dot,gammad1_dot,gammaq1_dot,zetad1_dot,zetaq1_dot,Pinv2_dot,Qinv2_dot,delta2_dot,ild2_dot,ilq2_dot,vod2_dot,voq2_dot,iod2_dot,ioq2_dot,gammad2_dot,gammaq2_dot,zetad2_dot,zetaq2_dot])
+    Yinv = Matrix([Pinv1,Pinv2,Qinv1,Qinv2])
+    Uinv = Matrix([vdref,vqref,w0])
+    Ainv,Binv = get_matrix(Xinv_dot,Xinv),get_matrix(Xinv_dot,Uinv)
+    #Cinv = get_matrix(Yinv,Xinv)
+    return Ainv,Binv
+
+def Linear_Inverter_Model(RL,XL,m1,m2,n1,n2,Kpc,Kic,Kpv,Kiv,X0):
+    w0i,wfi = np.pi*2*50,np.pi*4
+    Ainv,Binv= Inverter_Linear(RL,XL,m1,m2,n1,n2,Kpc,Kic,Kpv,Kiv)
+    pinv1si,qinv1si,delta1si,ild1si,ilq1si,vod1si,voq1si,iod1si,ioq1si,gammad1si,gammaq1si,zetad1si,zetaq1si,pinv2si,qinv2si,delta2si,ild2si,ilq2si,vod2si,voq2si,iod2si,ioq2si,gammad2si,gammaq2si,zetad2si,zetaq2si = X0
+
+    Amm = Ainv.subs([(iod1,iod1si),(iod2,iod2si),(ioq1,ioq1si),(ioq2,ioq2si),(vod1,vod1si),(voq1,voq1si),(vod2,vod2si),(voq2,voq2si)])
+    Amm = Amm.subs([(w0,w0i),(wf,wfi),(delta1,delta1si),(delta2,delta2si),(Pinv1,pinv2si),(Pinv2,pinv2si)])
+    Amm = Amm.subs([(ild1,ild1si),(ild2,ild2si),(ilq1,ilq1si),(ilq2,ilq2si)])
+
+    Bmm = Binv.subs([(ild1,ild1si),(ild2,ild2si),(ilq1,ilq1si),(ilq2,ilq2si)])
+    Bmm = Bmm.subs([(iod1,iod1si),(iod2,iod2si),(ioq1,ioq1si),(ioq2,ioq2si),(vod1,vod1si),(voq1,voq1si),(vod2,vod2si),(voq2,voq2si)])
+    return np.array(Amm).astype(np.float64),np.array(Bmm).astype(np.float64)
